4345ca5427b1f4dbf536a7879325a5d52d08e3b7
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-08-05 12:36:18 -0700

    scaffold all components & hello world

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Todo extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <>
+        <p>hello world</p>
+      </>
+    );
+  }
+}
+
+export default Todo;
